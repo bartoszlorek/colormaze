49354cc51ddbccd1cb4e18906b7ca812099198f0
Bartosz Lorek <bery.lorek@gmail.com>
2017-05-13 02:45:37 +0200

    add objects to scene by factory function

--- src/maze/renderer.js
+++ src/maze/renderer.js
@@ -1,52 +1,54 @@
+import { forEach } from 'lodash';
+
 export default renderer;
 
 function renderer(p, maze) {
     const { cols, rows } = maze;
     const scene = [];
-    let size;
 
-    const render = function () {
-        if (this.cell.solved) {
-            let { left, top } = this.bounds;
-            p.noStroke()
-                .fill('#222')
-                .rect(left, top, size, size);
-        }
-        p.stroke('#fff');
-        this.cell.walls.forEach((wall, i) => {
-            if (wall) {
-                p.line.apply(p, this.points[i]);
+    const proto = {
+        get bounds() {
+            let left = this.cell.x * size,
+                top = this.cell.y * size;
+            return {
+                top,
+                right: left + size,
+                bottom: top + size,
+                left
             }
-        });
-    };
+        },
+        get points() {
+            let { top, right, bottom, left } = this.bounds;
+            return [
+                [left, top, right, top],
+                [right, top, right, bottom],
+                [right, bottom, left, bottom],
+                [left, bottom, left, top]
+            ]
+        },
+        render: function () {
+            if (this.cell.solved) {
+                let { left, top } = this.bounds;
+                p.noStroke()
+                    .fill('#222')
+                    .rect(left, top, size, size);
+            }
+            p.stroke('#fff');
+            this.cell.walls.forEach((wall, i) => {
+                if (wall) {
+                    p.line.apply(p, this.points[i]);
+                }
+            });
+        }
+    }
 
     maze.each(cell => {
-        let body = {
-            cell,
-            render,
-            get bounds() {
-                let left = this.cell.x * size,
-                    top = this.cell.y * size;
-                return {
-                    top,
-                    right: left + size,
-                    bottom: top + size,
-                    left
-                }
-            },
-            get points() {
-                let { top, right, bottom, left } = this.bounds;
-                return [
-                    [left, top, right, top],
-                    [right, top, right, bottom],
-                    [right, bottom, left, bottom],
-                    [left, bottom, left, top]
-                ]
-            }
-        };
-        scene.push(body);
+        let obj = Object.create(proto);
+        Object.assign(obj, { cell });
+        scene.push(obj);
     });
 
+    let size;
     return (width, height) => {
         let x = (width || 100) / cols,
             y = (height || 100) / rows;
