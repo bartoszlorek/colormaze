463d9978f65057de3283ec171866f6e08e13a5cb
Bartosz Lorek <bery.lorek@gmail.com>
2017-05-15 23:02:16 +0200

    call multiple events by binding them to global loop

---
+++ src/action.js
@@ -0,0 +1,39 @@
+import { forEach, isArray } from 'lodash';
+import paper, { Key } from 'paper';
+
+const fps = 60/30;
+const events = {};
+let spins = false,
+    names = [];
+
+export default function action(key, callback) {
+    if (typeof callback !== 'function') {
+        throw 'second parameter must be a function';
+    }
+    if (isArray(key)) {
+        forEach(key, k => events[k] = callback);
+    } else {
+        events[key] = callback;
+    }
+    names = Object.keys(events);
+}
+
+action.spin = () => {
+    if (paper.view === null) {
+        return false;
+    }
+    spins = true;
+    paper.view.onFrame = function (e) {
+        if (spins) {
+            forEach(names, n => {
+                if (Key.isDown(n)) {
+                    events[n]();
+                }
+            });
+        }
+    }
+}
+
+action.stop = () => {
+    spins = false;
+}
